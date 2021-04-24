56b21a03f18b378b6ee3b713f51ce295e99fabd1
prakharjindal123 <77190220+prakharjindal123@users.noreply.github.com>
2021-04-24 19:41:35 +0530

    Add files via upload

---
+++ Boggie.js
@@ -0,0 +1,20 @@
+class Boggie{
+    constructor(x,y,width,height){
+      var options ={ isStatic: false}
+      
+      this.body =  Bodies.rectangle(x,y,width,height,options);
+      this.image = loadImage("images/coach.png");
+      World.add(myWorld,this.body);
+
+      Matter.Body.setMass(this.body,this.body.mass*5);
+      this.width = width;
+      this.height = height;
+    }
+    show()
+    {
+        imageMode(CENTER);
+        image(this.image,this.body.position.x, this.body.position.y,this.width, this.height);
+    }
+  
+  
+  }
